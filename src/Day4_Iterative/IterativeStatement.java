0edb9659058f8f704f620ab89d1f12cc4ccb5fd1
Abhinav kar <karabhinav24@gmail.com>
2024-06-25 13:10:07 +0530

    added Sum of Natural Number

---
+++ src/Day4_Iterative/IterativeStatement.java
@@ -0,0 +1,14 @@
+package Day4_Iterative;
+
+import java.sql.SQLOutput;
+
+public class IterativeStatement {
+    public static void main(String[] args) {
+//         int n = 10;
+//         for ( int i =0 ; i<n ; i++){
+//             System.out.println(i);
+//         }
+
+//        Print the Sum of
+    }
+}
---
+++ src/Day4_Iterative/sumOfNNaturalNumber.java
@@ -0,0 +1,12 @@
+package Day4_Iterative;
+
+public class sumOfNNaturalNumber {
+    public static void main(String[] args) {
+        int n =1000;
+        int sum =0;
+        for (int i =1 ; i<=n; i++){
+            sum+=i ;
+        }
+        System.out.println(sum);
+    }
+}
