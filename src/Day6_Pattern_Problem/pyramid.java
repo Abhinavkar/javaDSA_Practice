180fd23d7febad1c2fc8922fed248ee856c87cc8
Abhinav kar <karabhinav24@gmail.com>
2024-06-27 20:09:58 +0530

    Pyramid Problem

---
+++ src/Day6_Pattern_Problem/pyramid.java
@@ -0,0 +1,21 @@
+package Day6_Pattern_Problem;
+
+public class pyramid {
+    public static void main(String[] args) {
+        int n = 5 ;
+        int star=1 ;
+        int space = n-1;
+        for (int row=0;row<n;row++){
+            for (int space1 = 0 ;space1<space;space1++)
+            {
+                System.out.print(" ");
+            }
+            for(int star1 = 0;star1<star;star1++ ){
+                System.out.print( "*" );
+            }
+            star+=2;
+            space--;
+            System.out.println();
+        }
+    }
+}
