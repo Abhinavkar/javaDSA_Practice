7505d2994509cad67b5ac21231ae7b8ec5ff1c9e
Abhinav kar <karabhinav24@gmail.com>
2024-06-27 18:55:14 +0530

    TernaryOperator.java_added

---
+++ src/Day5_Iterative_Problem_Continued/ContinueStatement.java
@@ -0,0 +1,11 @@
+package Day5_Iterative_Problem_Continued;
+
+public class ContinueStatement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if(i>4&&i<9)
+                continue;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Day5_Iterative_Problem_Continued/TernaryOperator.java
@@ -0,0 +1,10 @@
+package Day5_Iterative_Problem_Continued;
+
+public class TernaryOperator
+{
+    public static void main(String[] args) {
+        int marks =75;
+        String result = (marks>70)?"pass":"fail";
+        System.out.println(result);
+    }
+}
