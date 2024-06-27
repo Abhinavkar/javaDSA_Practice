68c59ceb78c62a989d10d4ae8cacc9e4e5d364c9
Abhinav kar <karabhinav24@gmail.com>
2024-06-27 18:46:24 +0530

    Added_Fibonacci_Number

--- src/Day4_Iterative/PrimeNo.java
+++ src/Day4_Iterative/PrimeNo.java
@@ -1,10 +1,8 @@
 package Day4_Iterative;
-import java.util.*;
-import java.math.*;
 
 public class PrimeNo {
     public static void main(String[] args) {
-        int n = 5 ;
+        int n = 2457 ;
         byte flag=0 ;
         for(int i =2;i<=Math.sqrt(n);i++) {
             if (n % i == 0) {
