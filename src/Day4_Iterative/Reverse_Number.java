17c156e0056398765b5c89e4dd7f2ccb5fa504d1
Abhinav kar <karabhinav24@gmail.com>
2024-06-27 18:12:12 +0530

    Added-Reverse_Number

--- src/Day4_Iterative/Reverse_Number.java
+++ src/Day4_Iterative/Reverse_Number.java
@@ -1,12 +1,12 @@
 package Day4_Iterative;
 
-public class Reverse4DigitNumber {
+public class Reverse_Number {
     public static void main(String[] args) {
         int n =1234 ;
-        int rem = 0 ;
+
         int reversedNumber=0 ;
         while(n>0){
-            rem=n%10;
+            int rem=n%10;
             n=n/10;
             reversedNumber=reversedNumber*10+rem;
 
